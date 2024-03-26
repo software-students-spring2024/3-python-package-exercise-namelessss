400b75bf90d09cef295cf1d0b649a9067586c043
Jiahuita <jl11844@nyu.edu>
2024-03-25 23:06:15 -0400

    Added pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="new_york_minute",
-    version="1.0.0",
+    version="1.1.0",
     description="A text-based game set in New York City",
     author="Hannah Horiuchi, Jiahua Liao, Kevin Lin, Nicole Luzuriaga",
     author_email="hah8236@nyu.edu, jl11844@nyu.edu, kl3890@nyu.edu, nl2210@nyu.edu",
